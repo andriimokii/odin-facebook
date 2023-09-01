3305d6dcd389469662dca1e95bd4626191515c14
Andrii Mokii <andriimokii@gmail.com>
2023-09-01 17:37:18 +0300

    add annotate gem

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id              :bigint           not null, primary key
+#  acknowledged    :boolean          default(FALSE), not null
+#  notifiable_type :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  notifiable_id   :bigint           not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_notifications_on_notifiable  (notifiable_type,notifiable_id)
+#  index_notifications_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class NotificationTest < ActiveSupport::TestCase
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :bigint           not null, primary key
+#  bio        :string
+#  full_name  :string
+#  location   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class ProfileTest < ActiveSupport::TestCase
